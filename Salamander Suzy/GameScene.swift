398c691ba95f3e289f02e6dc01cb3c80c97f4b10
[myblog] <imanlintag13@mittymonarch.com>
2017-06-30 17:59:14 -0700

    Initialized ground size

--- Salamander Suzy/GameScene.swift
+++ Salamander Suzy/GameScene.swift
@@ -15,7 +15,11 @@ class GameScene: SKScene {
     private var spinnyNode : SKShapeNode?
     
     override func didMove(to view: SKView) {
-        backgroundColor = UIColor.blue
+        backgroundColor = UIColor(red: 159.0/255.0, green: 201.0/255.0, blue: 244/255.0, alpha: 1.0)
+        
+        let ground = SKSpriteNode(color: UIColor.brown, size: CGSize(width: 1000, height: 20))
+        ground.position = view.center
+        addChild(ground)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
---
+++ Salamander Suzy/MLMovingGround.swift
@@ -0,0 +1,16 @@
+//
+//  MLMovingGround.swift
+//  Salamander Suzy
+//
+//  Created by Iman Lintag on 6/30/17.
+//  Copyright © 2017 Iman Lintag. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class MLMovingGround {
+    init(size: CGSize) {
+
+    }
+}
